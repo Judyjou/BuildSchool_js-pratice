45feb206be957dc6903cd35ff286eaf9046d3531
aquariusjou@gmail.com <aquariusjou@gmail.com>
2020-04-14 21:13:29 +0800

    commit

---
+++ try.js
@@ -0,0 +1,22 @@
+let array = [2,5,3,1,4];
+array.sort(function(a,b){
+    return a-b;
+});
+console.log(array);
+
+var object = [{name:'apple',value:34},{name:'jou',value:22},{name:'cat',value:4}];
+object.sort(function(a,b){
+    return a.value-b.value;
+});
+console.log(object);
+
+var object = [{name:'a',value:34},{name:'j',value:22},{name:'c',value:4}];
+object.sort(function(a,b){
+    var nameA=a.name.toUpperCase();
+    var nameB=b.name.toUpperCase();
+    if (nameA < nameB){
+        return -1;
+    }
+    // return a.name-b.name;
+});
+console.log(object);
